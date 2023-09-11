e8d9a5e726bf9bd58cc85ca2040b5cd52520ce6f
sakhawat71 <sakhawat.showrav@gmail.com>
2023-09-11 18:16:58 +0600

    header-complete

---
+++ custom-config.js
@@ -0,0 +1,12 @@
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        'bike-btn-bg': '#E76F51',
+        'bike-slid-bg': '#E76F511A',
+      }
+    }
+  }
+}
